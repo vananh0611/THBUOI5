fc56cd35dfc607c5b93f39c3cc090a59160d82ab
vananh0611 <van95052@gmail.com>
2023-08-17 14:28:46 +0700

    first commit

---
+++ google_map-Starting/lib/constants.dart
@@ -0,0 +1,7 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+const String google_api_key = "AIzaSyDBGaNN0e-7DBNEM1gFf9U9Rd6tKoNOi0g";
+const Color primaryColor = Color(0xFF7B61FF);
+const double defaultPadding = 16.0;
